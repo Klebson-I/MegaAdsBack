e746b01b43e7b1fa21f9ede2517b624381aaa168
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-05-16 10:49:38 +0200

    Create validation for record

--- records/ad.record.ts
+++ records/ad.record.ts
@@ -1,11 +1,40 @@
+import {AdEntity} from "../types";
+import {ValidationError} from "../utils/utils";
 
+interface NewAdEntity extends Omit<AdEntity,'id'> {
+    id?:string;
+}
 
+export class AdRecord implements AdEntity{
 
+    id:string;
+    name:string;
+    description:string;
+    price:number;
+    url:string;
+    lat:number;
+    lon:number;
 
-export class AdRecord {
+    constructor(obj:NewAdEntity) {
+        if (!obj.name || obj.name.length>100) {
+            throw new ValidationError("Lack of name or name too long");
+        }
 
-    constructor() {
+        if (obj.description.length>1000) {
+            throw new ValidationError("Ad description cannot has more than 1000 chars");
+        }
 
+        if (obj.price<0 || obj.price > 9999999) {
+            throw new ValidationError("Price cannot be less than 0 and higher than 9999999");
+        }
+
+        if (!obj.url || obj.url.length > 0) {
+            throw new ValidationError("Link to ad cannot be empty or has move than 100 chars");
+        }
+
+        if(obj.lat ||obj.lon){
+            throw new ValidationError("Cannot localize ad");
+        }
     }
 
 }
